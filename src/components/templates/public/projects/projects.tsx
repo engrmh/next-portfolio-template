3cc6abbcd2a0ef31f0102c046508f99076ba38f7
engrmh <59095451+engrmh@users.noreply.github.com>
2025-05-29 17:18:06 +0330

    Update projects.tsx

--- src/components/templates/public/projects/projects.tsx
+++ src/components/templates/public/projects/projects.tsx
@@ -8,7 +8,12 @@ export default function Projects() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {siteConfig.projects.map((p, i) => (
-        <CardBoxContainer cmd={`show project ${i + 1}`} className="" key={i}>
+        <CardBoxContainer
+          cmd={`show project ${i + 1}`}
+          className=""
+          key={i}
+          delay="100"
+        >
           <Image
             src={`/images/projects/${p.img}`}
             width={300}
